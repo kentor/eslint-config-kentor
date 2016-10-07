134bcdf852fce2d23977c7833fa788a23795fd27
Kenneth Chung <ken70r@gmail.com>
2016-10-06 23:18:47 -0700

    wrap-multilines -> jsx-wrap-multilines
    address rule deprecation

--- react.js
+++ react.js
@@ -16,10 +16,10 @@ module.exports = {
     'react/jsx-no-undef': 2,
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
+    'react/jsx-wrap-multilines': 2,
     'react/no-unknown-property': 2,
     'react/react-in-jsx-scope': 2,
     'react/self-closing-comp': 2,
     'react/sort-comp': 2,
-    'react/wrap-multilines': 2,
   },
 };
