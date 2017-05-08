662da00e07414e0fd03c89cad7d90249f68fb039
Kenneth Chung <ken70r@gmail.com>
2017-05-07 19:03:29 -0700

    fix bad config file
    woops

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ module.exports = {
     'no-underscore-dangle': 0,
     'no-unneeded-ternary': 2,
     'no-whitespace-before-property': 2,
-    'nonblock-statement-body-position'
+    'nonblock-statement-body-position': 2,
     'object-curly-newline': 0,
     'object-curly-spacing': [2, 'always'],
     'object-property-newline': 0,
